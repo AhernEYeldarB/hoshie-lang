5e546d445af605585c55c1000ab11617e8bc8262
AhernEYeldarB <ahernebradley@gmail.com>
2022-04-11 01:13:57 +0100

    WIP link demo with HL library

--- docs/pages.js
+++ docs/pages.js
@@ -1,80 +1,57 @@
 import { Observable } from "@hpcc-js/observable-md";
 import { LitElement, html } from "lit";
 import { ECLEditor } from "@hpcc-js/codemirror";
+const generate = window.generate;
 
 // Init initial state
 const code = `\
-//  Short form function
-add = (number a, number b) => a + b;
-
-//  Long form function
-add2 = (number a, number b) => {
-    return a + b;
+Person = {
+  string fname,
+  string lname,
+  number age,
+  string job,
+  string zipcode,
+  string state
 };
 
-//  Function with locally scoped declarations
-add3 = (number a, number b) => {
-    tmpA = a;
-    tmpB = b;
-    tmp = tmpA + tmpB;
-    return tmp;
-};
+data = [
+{
+  "Santiago",
+  "Kuvalis",
+  36,
+  "Designer",
+  "20522-7262",
+  "MA"
+},
+{
+  "Cornelius",
+  "Hyatt",
+  58,
+  "Engineer",
+  "97368-0001",
+  "MT"
+},
+{
+  "Gino",
+  "Blanda",
+  35,
+  "Facilitator",
+  "36178-3400",
+  "ND"
+},
+{
+  "Kameron",
+  "Huel",
+  60,
+  "Engineer",
+  "88002-1751",
+  "TX"
+}
+] typeof Person[];
 
-add(1, 2);
-add2(1, 2);
-add3(1, 2);
-
-//  Function with default value support
-join = (string a = "Hello", string b = "World") => a + " " + b;
-
-join();
-join("Goodbye");
-join(, "Earth");
-join("Goodbye", "Earth");
+sortByAge = pipeline(sort((Person l, Person r) => l.age - r.age));
+sortByAge(data);
 `;
-
-// Initialise app
-var editor = new ECLEditor()
-  .ecl(code)
-  .target("editor")
-  .render()
-  ;
-
-var app = new Observable()
-.target("render")
-.showValues(true)
-.mode("omd")
-.text(
-  " \n" +
-  "``` " +
-  "df = require(\"@hpcc-js/dataflow\"); \n"+
-  "add = (a, b) => { \n" +
-  "  return a + b; \n" +
-  "} \n" +
-  "add2 = (a, b) => { \n" +
-  "  return a + b; \n" +
-  "} \n" +
-  "add3 = (a, b) => { \n" +
-  "  var tmpA = a \n" +
-  "  var tmpB = b \n" +
-  "  var tmp = tmpA + tmpB \n" +
-  "  return tmp \n" +
-  "} \n" +
-  "join = (a, b) => { \n" +
-  "  a = a !== undefined ? a : 'Hello' \n" +
-  "  b = b !== undefined ? b : 'World' \n" +
-  "  return a + ' ' + b \n" +
-  "} \n" +
-  "add(1, 2) \n" +
-  "add2(1, 2) \n" +
-  "add3(1, 2) \n" +
-  "join() \n" +
-  "join(\"Goodbye\") \n" +
-  "join(undefined, 'Earth') \n" +
-  "join('Goodbye', 'Earth') \n" +
-  "\n" +
-  "``` \n"
-);
 
 // Utils
 const doResize = () => {
@@ -86,11 +63,43 @@ const doResize = () => {
   }
 };
 
+const convertToObservablemdString = (text) => {
+  return text.replaceAll(/const /g, "");
+};
+
+// Initialise app
+var editor = new ECLEditor()
+  .ecl(code)
+  .target("editor")
+  .render()
+  ;
+
+var app = new Observable()
+  .target("render")
+  .showValues(true)
+  .mode("omd")
+  .text(
+    " \n" +
+    "``` " +
+    `${convertToObservablemdString(generate(code))}` +
+    "``` \n"
+  );
+
+editor._codemirror.on("change", (editor) => {
+  const observablemdJs = convertToObservablemdString(generate(editor.getValue()));
+  const textToRender = "" +
+    "``` \n" +
+    `${observablemdJs} \n` +
+    "```";
+  app._text = textToRender;
+  app._lazyRender();
+});
+
 // Components
 class ErrorTab extends LitElement {
 
   constructor() {
-    super();  
+    super();
     this.app = app;
     this._timerInterval = setInterval(() => this.requestUpdate(), 2000);
   }
@@ -98,12 +107,14 @@ class ErrorTab extends LitElement {
   render() {
     return html`
     <table>
-      ${this.app._errors.map((error) => { return error.severity === "error" ? html`
+      ${this.app._errors.map((error) => {
+      return error.severity === "error" ? html`
       <tr>
         <td>${error.name}</td>
         <td>${error.message}</td>
       </tr>
-      ` : ""; })}
+      ` : "";
+    })}
     </table>
     `;
   }
---
+++ src/hlcc/codeGen/genBodyless.ts
@@ -0,0 +1,310 @@
+import { AdditiveExpression, ArrayExpression, ArrowBody, ArrowParamater, DataExpression, EqualityExpression, FunctionCallExpression, IdentifierExpression, LogicalExpression, MultiplicativeExpression, NotExpression, NumericExpression, RelationalExpression, StringExpression, UnaryMinusExpression } from "../cst/expression";
+import { CountFunction, FilterFunction, FirstNFunction, GenerateFunction, GroupCountFunction, GroupFunction, LengthFunction, PipelineFunction, RandomFunction, ReadJsonFunction, SortFunction, WriteJsonFunction } from "../cst/function";
+import { HLScope, resolveRef } from "../cst/scope";
+import { HLFileScope } from "../cst/scopes/file";
+import { HLFunctionScope } from "../cst/scopes/function";
+import { RowType } from "../cst/types";
+
+type Declaration = { [id: string]: string };
+type FileContent = { [path: string]: { declarations: Declaration, actions: string[] } };
+
+class JSWriter {
+    fileContent: FileContent = {};
+    hasOutput: boolean = false;
+
+    append(line: string, path: string) {
+        if (!this.fileContent[path]) {
+            this.fileContent[path] = { declarations: {}, actions: [] };
+        }
+        this.fileContent[path].actions.push(line);
+    }
+
+    outputDecl(path: string) {
+        const retVal: string[] = [];
+        for (const decl in this.fileContent[path].declarations) {
+            retVal.push(`const ${decl} = ${this.fileContent[path].declarations[decl]};`);
+        }
+        return retVal.join("\n");
+    }
+
+    outputBuffer(path: string) {
+        return this.fileContent[path].actions.join("\n");
+    }
+
+    output() {
+        const retVal: string[] = [];
+        for (const hoPath in this.fileContent) {
+            this.hasOutput = true;
+            // const outPath = hoPath.split(".");
+            // outPath.pop();
+            // outPath.push("js");
+            // const jsPath = path.join("out-js", outPath.join("."));
+            // const jsPath = outPath(hoPath);
+            // retVal.push(`// ${jsPath}`);
+            const content = `\
+/* eslint-disable */
+const df = require("@hpcc-js/dataflow");
+
+${this.outputDecl(hoPath)}
+
+${this.outputBuffer(hoPath)}
+`;
+            // fs.mkdirSync(path.dirname(jsPath), { recursive: true });
+            // fs.writeFileSync(jsPath, content);
+            retVal.push(content);
+        }
+        return retVal.join("\n");
+    }
+
+    generate(row: any) {
+        if (typeof row === "string") {
+            // debugger;
+            return row;
+        } else if (typeof row === "undefined") {
+            return "//  generate(undefined)";
+        } else if (this[row?.constructor?.name]) {
+            return this[row.constructor.name](row);
+        }
+        return `todo("${row?.constructor?.name}")`;
+    }
+
+    writeAction(row: any) {
+        const text = this.generate(row);
+        const ref = resolveRef(row);
+        if (text !== undefined) {
+            if (ref?.func instanceof PipelineFunction) {
+                this.append(`JSON.stringify([...${text}], undefined, 2);`, row.scope.path);
+            } else if (row instanceof WriteJsonFunction) {
+                this.append(`${text};`, row.scope.path);
+            } else if (ref?.isActivity) {
+                this.append(`JSON.stringify(${text}.peek(), undefined, 2);`, row.scope.path);
+            } else if (ref?.isSensor) {
+                this.append(`JSON.stringify(${text}.peek(), undefined, 2);`, row.scope.path);
+            } else if (ref.type === "data" || ref.type?.indexOf("[]") >= 0) {
+                this.append(`JSON.stringify(${text}, undefined, 2);`, row.scope.path);
+            } else {
+                this.append(`${text};`, row.scope.path);
+            }
+            return true;
+        }
+        console.log(`Unhandled type:  ${row?.constructor?.name}`);
+        return false;
+    }
+
+    writeDecl(id: string, ref: any, scope: HLScope) {
+        if (scope instanceof HLFunctionScope) {
+            // debugger;
+        }
+        if (!this.fileContent[scope.path]) {
+            this.fileContent[scope.path] = { declarations: {}, actions: [] };
+        }
+        if (!this.fileContent[scope.path].declarations[id]) {
+            this.fileContent[scope.path].declarations[id] = this.generate(ref);
+        }
+    }
+
+    IdentifierExpression(row: IdentifierExpression) {
+        if (row.scope instanceof HLFileScope) {
+            this.writeDecl(row.id, row.ref, row.scope);
+        }
+        return `${row.id}`;
+    }
+
+    BooleanExpression(row: NumericExpression) {
+        return `${row.value}`;
+    }
+
+    NumericExpression(row: NumericExpression) {
+        return `${row.value}`;
+    }
+
+    StringExpression(row: StringExpression) {
+        return `"${row.value}"`;
+    }
+
+    NotExpression(row: NotExpression) {
+        return `!${this.generate(row.expression)}`;
+    }
+
+    UnaryMinusExpression(row: UnaryMinusExpression) {
+        return `-${this.generate(row.expression)}`;
+    }
+
+    DataExpression(row: DataExpression) {
+        const fields = row.fields.map((exp, i) => {
+            const id = row.rowType?.fields[i]?.id || i;
+            return `${id}: ${this.generate(exp)}`;
+        });
+        return `{ ${fields.join(", ")} }`;
+    }
+
+    ArrayExpression(row: ArrayExpression) {
+        const fields = row.values.map((exp, i) => {
+            if (exp instanceof DataExpression && row.rowType?.rowType) {
+                exp.typeInfo(row.rowType?.rowType as RowType);
+            }
+            return `${this.generate(exp)}`;
+        });
+        return `[ ${fields.join(", ")} ]`;
+    }
+
+    AdditiveExpression(row: AdditiveExpression) {
+        return `${this.generate(row.lhs)} ${row.action} ${this.generate(row.rhs)}`;
+    }
+
+    MultiplicativeExpression(row: MultiplicativeExpression) {
+        return `${this.generate(row.lhs)} ${row.multiplicative} ${this.generate(row.rhs)}`;
+    }
+
+    RelationalExpression(row: RelationalExpression) {
+        return `${this.generate(row.lhs)} ${row.action} ${this.generate(row.rhs)}`;
+    }
+
+    LogicalExpression(row: LogicalExpression) {
+        return `${this.generate(row.lhs)} ${row.action} ${this.generate(row.rhs)}`;
+    }
+
+    EqualityExpression(row: EqualityExpression) {
+        return `${this.generate(row.lhs)} ${row.action} ${this.generate(row.rhs)}`;
+    }
+
+    FunctionCallExpression(row: FunctionCallExpression) {
+        this.writeDecl(row.id, row.func, row.scope);
+        row.args.forEach(arg => this.generate(arg));
+        return `${row.id}(${row.args.map(arg => {
+            return arg?.ctx.getText() || "undefined";
+        }).join(", ")})`;
+    }
+
+    HLFunctionScope(row: HLFunctionScope) {
+
+        const defaults = [];
+        row.params.forEach(param => {
+            this.generate(param);
+            if (param.defaultExpression()) {
+                defaults.push(`${param.id} = ${param.id} !== undefined ? ${param.id} : ${this.generate(param.defaultExpression())};`);
+            }
+        });
+
+        return `(${row.params.map(param => param.id).join(", ")}) => {
+${defaults.join("\n")}${defaults.length ? "\n" : ""}\
+${this.generate(row.body)}
+}`;
+    }
+
+    ArrowParamaterXXX(row: ArrowParamater) {
+    }
+
+    ArrowBody(row: ArrowBody) {
+        return `\
+${row.items?.map(item => this.generate(item)).join(";\n")}${row.items?.length ? ";\n" : ""}\
+${row.returnExpression ? "return" : ""} ${this.generate(row.returnExpression)};`;
+    }
+
+    Declaration(row: Declaration) {
+        return `${row.id} = ${this.generate(row.expression)}`;
+    }
+
+    GenerateFunction(row: GenerateFunction) {
+        return `df.generate(${this.generate(row.expression)}, ${this.generate(row.total)})`;
+    }
+
+    RandomFunction(row: RandomFunction) {
+        return "TODO"; //`Math(${this.generate(row.expression)}, ${this.generate(row.total)})`;
+    }
+
+    PipelineFunction(row: PipelineFunction) {
+        return `df.pipe(${row.items.map(item => {
+            const retVal = this.generate(item);
+            const ref = resolveRef(item);
+            if (ref?.isSensor) {
+                return `df.sensor(${retVal})`;
+            }
+            return retVal;
+        }).join(", ")})`;
+    }
+
+    LengthFunction(row: LengthFunction) {
+        const ref = resolveRef(row.expression);
+        return `${(row.expression as any).ctx.getText()}.length`;
+    }
+
+    FilterFunction(row: FilterFunction) {
+        return `df.filter(${this.generate(row.expression)})`;
+    }
+
+    FirstNFunction(row: FirstNFunction) {
+        return `df.first(${row.count})`;
+    }
+
+    SkipNFunction(row: FirstNFunction) {
+        return `df.skip(${row.count})`;
+    }
+
+    GroupFunction(row: GroupFunction) {
+        return `df.group(${this.generate(row.expression)})`;
+    }
+
+    GroupCountFunction(row: GroupCountFunction) {
+        return `df.pipe(df.group(${this.generate(row.expression)}), df.map(row => ({key: row.key, value: row.value.length})))`;
+    }
+
+    SortFunction(row: SortFunction) {
+        return `df.sort(${this.generate(row.expression)})`;
+    }
+
+    MapFunction(row: SortFunction) {
+        return `df.map(${this.generate(row.expression)})`;
+    }
+
+    CountFunction(row: CountFunction) {
+        return "df.count()";
+    }
+
+    DeviationFunction(row: CountFunction) {
+        return `df.deviation(${this.generate(row.expression)})`;
+    }
+
+    DistributionFunction(row: CountFunction) {
+        return `df.distribution(${this.generate(row.expression)})`;
+    }
+
+    ExtentFunction(row: CountFunction) {
+        return `df.extent(${this.generate(row.expression)})`;
+    }
+
+    MaxFunction(row: CountFunction) {
+        return `df.max(${this.generate(row.expression)})`;
+    }
+
+    MeanFunction(row: CountFunction) {
+        return `df.mean(${this.generate(row.expression)})`;
+    }
+
+    MedianFunction(row: CountFunction) {
+        return `df.median(${this.generate(row.expression)})`;
+    }
+
+    MinFunction(row: CountFunction) {
+        return `df.min(${this.generate(row.expression)})`;
+    }
+
+    QuartileFunction(row: CountFunction) {
+        return `df.quartile(${this.generate(row.expression)})`;
+    }
+
+    VarianceFunction(row: CountFunction) {
+        return `df.variance(${this.generate(row.expression)})`;
+    }
+}
+
+export function generate(text: string ) {
+    const hlFile = new HLFileScope("", "", text);
+    const jsWriter = new JSWriter();
+    hlFile.allActions().forEach(row => {
+        jsWriter.writeAction(row);
+    });
+    return jsWriter.output();
+}
+
+window.generate = generate;
